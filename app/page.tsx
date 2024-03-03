6a13ad048234cc8ef030b4d29cfead0ce224a741
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-03-03 22:33:37 +0900

    Modalを追加

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import CardList from "./components/CardList";
 
 export default function Home() {
   return (
-    <main className="px-10">
+    <main className="px-10 py-3">
       <div className="text-3xl">
         <h2>page1</h2>
       </div>
