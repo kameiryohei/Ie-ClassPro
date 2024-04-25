f57bdd0a9d37fe223040dc0bb7eb34d9907246ff
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-04-25 09:35:49 +0900

    READMEを更新

--- app/components/Session.tsx
+++ app/components/Session.tsx
@@ -22,7 +22,7 @@ const HomeSession = () => {
             <Link href="/user/register">新規登録する</Link>
           </button>
           <button className="bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded-xl duration-200">
-            <Link href="/user/register">ログインする</Link>
+            <Link href="/user/login">ログインする</Link>
           </button>
         </div>
       )}
