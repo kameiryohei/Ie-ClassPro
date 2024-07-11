162058e6b7438c1fa708aef7c826142678d8017b
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-07-11 22:06:25 +0900

    不要なAPIを削除

--- app/api/post/allpost/route.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "@/utils/prisma/prismaClient";
-import { NextResponse } from "next/server";
-
-//投稿を全て取得するAPI
-export async function GET(req: Request) {
-  const allPosts = await prisma.post.findMany();
-  return NextResponse.json(allPosts);
-}
