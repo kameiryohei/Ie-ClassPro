abb40b36ed468330c4a7b3a7dc74a2ac88e83a09
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-10-13 17:29:32 +0900

    next.jsのmiddlewareを編集してログインしていない時のアクセス制限を実施

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,39 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { updateSession } from "utils/supabase/middleware";
 
-export function middleware(req: NextRequest) {
-  // APIへのアクセスに対してのみミドルウェアを適用
-  if (req.nextUrl.pathname.startsWith("/api")) {
-    // x-api-keyの検証
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  // updateSession を除外するパスのリスト
+  const excludeSessionUpdatePaths = ["/"];
+
+  // `/api` パスに対してのみ API 保護の処理を適用
+  if (pathname.startsWith("/api")) {
     const apiKey = req.headers.get("x-api-key");
     if (apiKey !== process.env.NEXT_PUBLIC_API_KEY) {
       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    return NextResponse.next();
+  }
+
+  // excludeSessionUpdatePaths に含まれないパス、かつ /allPost で始まらないパスに対してのみ updateSession を適用
+  if (
+    !excludeSessionUpdatePaths.includes(pathname) &&
+    !pathname.startsWith("/allPost")
+  ) {
+    const response = await updateSession(req);
+    if (response) {
+      return response;
     }
   }
 
   return NextResponse.next();
 }
 
-// ミドルウェアの適用範囲を指定
 export const config = {
-  matcher: "/api/:path*", // すべてのAPIエンドポイントに適用
+  matcher: [
+    "/api/:path*",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
