49ba335e06b9d8714696b048d6b2b5cb8bf1f4ed
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-10-15 17:59:23 +0900

    topの余白を調整

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,7 +2,7 @@ import ProfileContent from "./components/ProfileContent";
 
 const ProfilePage = () => {
   return (
-    <div className="mx-auto max-w-3xl lg:max-w-2xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32">
+    <div className="mx-auto max-w-3xl lg:max-w-2xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-22">
       <h1 className="text-2xl font-bold mb-5">プロフィール画面</h1>
       <div className="bg-slate-50 rounded-xl px-4 py-4 shadow-lg ring-1 ring-gray-400">
         <ProfileContent />
