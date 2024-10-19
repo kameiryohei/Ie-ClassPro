ed7d7abe01089d9e2e9b7d4b69169651b802e4ec
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-10-19 19:55:43 +0900

    LPのログイン時、ログインしてない時のUIを分岐

--- app/components/Session.tsx
+++ app/components/Session.tsx
@@ -1,14 +1,13 @@
-"use client";
 import Link from "next/link";
 import React from "react";
-import useUser from "../hooks/useUser";
+import useSeverUser from "app/hooks/useSeverUser";
 
-const HomeSession = () => {
-  const { session } = useUser();
-
+const HomeSession = async () => {
+  const { session } = useSeverUser();
+  const id = await session();
   return (
     <div>
-      {session ? (
+      {id ? (
         <div className="pt-6 flex justify-center">
           <Link href="/allPost">
             <button className="px-4 py-2 bg-orange-500 text-white rounded-2xl">
@@ -18,12 +17,11 @@ const HomeSession = () => {
         </div>
       ) : (
         <div className="pt-6 flex justify-center gap-8">
-          <button className="bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded-xl duration-200">
-            <Link href="/user/register">新規登録する</Link>
-          </button>
-          <button className="bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded-xl duration-200">
-            <Link href="/user/login">ログインする</Link>
-          </button>
+          <Link href="/login">
+            <button className="px-4 py-2 bg-orange-500 text-white rounded-2xl">
+              ログイン or 新規登録する
+            </button>
+          </Link>
         </div>
       )}
     </div>
