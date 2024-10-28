699dc5c210053b01bb18004400c8ccff9760202a
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-10-28 22:40:16 +0900

    profileのカードにテストを記述

---
+++ __test__/mock/mock_Profile.ts
@@ -0,0 +1,12 @@
+import { Profile } from "app/profile/components";
+
+export const mockProfile: Profile = {
+  name: "Test User",
+  email: "test@example.com",
+  university: "Test University",
+  faculty: "Test Faculty",
+  department: "Test Department",
+  grade: 3,
+  id: 0,
+  auth_id: "ffffff",
+};
