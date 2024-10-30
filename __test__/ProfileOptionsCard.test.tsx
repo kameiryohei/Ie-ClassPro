ff3f583a056e5e05c264b9c448112c76b667d623
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-10-30 15:14:42 +0900

    不要なコメント削除

--- __test__/ProfileOptionsCard.test.tsx
+++ __test__/ProfileOptionsCard.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { useRouter } from "next/navigation";
 import ProfileOptionsCard from "app/profile/components/ProfileOptionsCard";
-//テス語がpassするけど、エラーが出る
 jest.mock("next/navigation", () => ({
   useRouter: jest.fn(),
 }));
